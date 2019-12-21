3afc71775dc3d58558021bd3a8baa79a66ac1592
Phil Polo <philip.polo@yahoo.com>
2019-12-21 17:51:15 -0600

    Pug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -16,8 +17,13 @@ const reviewRouter = require('./routes/reviewRoutes');
 const app = express();
 
 app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 // global middlewares
+
+// serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // security http headers
 app.use(helmet());
 
@@ -55,9 +61,6 @@ app.use(
   })
 );
 
-// serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -67,6 +70,13 @@ app.use((req, res, next) => {
 });
 
 //routes
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Phil'
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
